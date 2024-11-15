80b441afef7a02ad0e864d95475855d97bcdbb1d
rdaguas <aguas.rdv2001@gmail.com>
2024-11-15 21:17:16 +0000

    Tarea4

---
+++ js/externotarea.js
@@ -0,0 +1,19 @@
+function cambiarColorTexto() {
+    document.getElementById('parrafo').style.color = 'blue';
+}
+
+function cambiarTamanioNombre() {
+    document.getElementById('nombre').style.fontSize = '25px';
+}
+
+function cambiarColorFondo() {
+    document.getElementById('fondo').style.backgroundColor = 'rgba(17, 68, 119, 0.3)';
+}
+
+function cambiarTamanioTexto() {
+    document.getElementById('parrafo').style.fontSize = '20px';
+}
+
+function agregarNuevoMenu() {
+    document.getElementById('idMenu').innerHTML += '<li>Soporte</li>';
+}
