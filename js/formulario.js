7b331959e17fafa39eff4d92bb2dd650eea57396
rdaguas <aguas.rdv2001@gmail.com>
2024-12-11 06:19:02 +0000

    Tarea7

---
+++ js/formulario.js
@@ -0,0 +1,28 @@
+const listas = []
+
+const app = Vue.createApp({
+    methods: {
+        agregarLista() {
+            const nuevaLista = {
+                nombre: this.nombre,
+                apellido: this.apellido,
+                Hobby: this.Hobby,
+                fechaNacimiento: this.fechaNacimiento,
+               
+            }
+            this.listaformulario.push(nuevaLista)
+        }
+    },
+
+    data() {
+        return {
+            listaformulario: listas,
+            nombre: null,
+            apellido: null,
+            Hobby: null,
+            fechaNacimiento: null,
+        }
+    }
+})
+
+app.mount('#myAppF')
